32b93f18336f13a824fdf5c02c5116f146888655
ssalgaonkar <ssalgaonkar@ccs.neu.edu>
2018-04-12 01:38:00 -0400

    user access control completed

--- src/plan/service.js
+++ src/plan/service.js
@@ -1,23 +1,45 @@
 // Load the SDK for JavaScript
+import {getAllPlansByUser as accessValidatedPlans} from "../auth/service";
+import each from 'async/each';
+
 let AWS = require('aws-sdk');
+AWS.config.region = 'us-east-1'; // Region
+
+let s3 = new AWS.S3();
 
 let getAllPlansByUser = (req, res) => {
-    // Create S3 service object
-    let s3 = new AWS.S3({apiVersion: '2006-03-01'});
-
-// Create the parameters for calling createBucket
-    let bucketParams = {
-        Bucket: 'dma-mission-plans'
-    };
-
-    // Call S3 to create the bucket
-    s3.listObjects(bucketParams, function (err, data) {
+    accessValidatedPlans((err, data) => {
         if (err) {
             console.log("Error", err);
             res.status(404).send(err);
         } else {
-            res.status(200).send(data);
+            data.Contents.shift();
+            each(data.Contents, (content, callback) => {
+                setPreSignedURL(content, callback);
+            }, (err) => {
+                // if any of the file processing produced an error, err would equal that error
+                if (err) {
+                    // One of the iterations produced an error.
+                    // All processing will now stop.
+                    console.log("Error", err);
+                    res.status(404).send(err);
+                } else {
+                    res.status(200).send(data);
+                }
+            });
         }
+    });
+};
+
+let setPreSignedURL = (content, callback) => {
+    let params = {
+        Bucket: 'drone-mission-plans',
+        Key: content.Key
+    };
+
+    s3.getSignedUrl('getObject', params, (err, url) => {
+        content.preSignedURL = url;
+        callback();
     });
 };
 
