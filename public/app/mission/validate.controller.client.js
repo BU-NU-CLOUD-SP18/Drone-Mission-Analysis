d46f8e8a1a8872388920cf9f3b4e0566dbd0973e
Gaurav Nitin Gandhi <gaurav.g101@gmail.com>
2018-04-13 12:03:46 -0400

    Comparison algorithm: handled negative headings

--- public/app/mission/validate.controller.client.js
+++ public/app/mission/validate.controller.client.js
@@ -63,15 +63,18 @@
                     let lat = (EXIF.getTag(this, "GPSLatitude") + "").split(",");
                     let long = (EXIF.getTag(this, "GPSLongitude") + "").split(",");
                     let alt = (EXIF.getTag(this, "GPSAltitude") + "");
-                    console.log("In or out?", lat, long, alt);
+                    //let heading = (EXIF.getTag(this, "GPSImgDirection") + "");
+                    //let pitch  = (EXIF.getTag(this, ""))
+                    console.log("In or out?", lat, long, alt, heading);
+                    metadata.push({"latitude": lat, "longitude": long, "altitude" : alt});
                 });
             });
-            metadata.push({"latitude": 42.33966264, "longitude": -71.09559111, "altitude": 30});
+            /*metadata.push({"latitude": 42.33966264, "longitude": -71.09559111, "altitude": 30});
             metadata.push({"latitude": 42.34041702, "longitude": -71.09353563, "altitude": 30});
             metadata.push({"latitude": 42.33961765, "longitude": -71.0929484, "altitude": 30});
             metadata.push({"latitude": 42.339007, "longitude": -71.09336146, "altitude": 30});
             metadata.push({"latitude": 42.33872151, "longitude": -71.09411785, "altitude": 30});
-            metadata.push({"latitude": 42.33908631, "longitude": -71.09517464, "altitude": 30});
+            metadata.push({"latitude": 42.33908631, "longitude": -71.09517464, "altitude": 30});*/
             return metadata;
         }
 
