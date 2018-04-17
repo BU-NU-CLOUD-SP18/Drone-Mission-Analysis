9681f07a230f90a00f1b0601d59122e98387db6b
rachanatondare <tondare.r@husky.neu.edu>
2018-04-17 16:44:30 -0400

    Visualization of Drone Mission Path

--- public/app/maps.controller.js
+++ public/app/maps.controller.js
@@ -5,21 +5,46 @@
 
     function mapsController(MissionService, $routeParams, $interval) {
       var vm = this;
+//key= AIzaSyA-ZohFkSUCflhvo63mWeHaup64mltv7Go
 
-      vm.positions =[
-        [40.71, -74.21], [40.72, -74.20], [40.73, -74.19], [40.74, -74.18],
-        [40.75, -74.17], [40.76, -74.16], [40.77, -74.15], [40.77, -74.15]
+      var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 10,
+          center: {lat: 40.74, lng: -74.18},
+          mapTypeId: 'terrain'
+      });
+
+      var map1 = new google.maps.Map(document.getElementById('map1'), {
+        zoom: 10,
+        center: {lat: 40.74, lng: -74.18},
+        mapTypeId: 'terrain'
+    });
+
+      var flightPlanCoordinates =[
+        {lat: 40.71, lng: -74.21},
+        {lat: 40.77, lng:-74.15},
+        {lat: 40.77, lng:-74.00},
+        {lat: 40.71, lng: -74.21},
       ];
 
-      $interval(function() {
-        var numMarkers = Math.floor(Math.random() * 4) + 4; //between 4 to 8 markers
-        vm.positions = [];
-        for (i = 0; i < numMarkers; i++) {
-          var lat = 40.71 + (Math.random() / 100);
-          var lng = -74.21 + (Math.random() / 100);
-          vm.positions.push([lat, lng]);
-        }
-      }, 2000);
+      
+      var flightPath = new google.maps.Polyline({
+        path: flightPlanCoordinates,
+        geodesic: true,
+        strokeColor: '#FF0000',
+        strokeOpacity: 1.0,
+        strokeWeight: 2
+      });
+
+      var flightPath1 = new google.maps.Polyline({
+        path: flightPlanCoordinates,
+        geodesic: true,
+        strokeColor: 'blue',
+        strokeOpacity: 1.0,
+        strokeWeight: 2
+      });
+
+      flightPath.setMap(map);
+      flightPath1.setMap(map1);
 
     }
 })();
